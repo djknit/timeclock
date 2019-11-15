8ec660b96eda677867ed5069eb1b82766edbdde5
David <djknit@gmail.com>
2019-11-15 01:40:27 -0600

    temporary front end static page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import logo from './logo512.png';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          App coming soon...
+        </p>
+        <a
+          className="App-link"
+          href="https://djknit.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Developed by David Knittel
+        </a>
+      </header>
     </div>
   );
 }
