854c711e8b294cdabd228a402bb2238d48382f6b
David <djknit@gmail.com>
2020-05-24 00:27:19 -0500

    created shared resources folder for use by both client and server

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,9 @@ import MainApp from './components/MainApp';
 import NotFound from './components/NotFound';
 import Footer from './components/Footer';
 
+import sharedResources from 'timeclock-shared-resources';
+console.log(sharedResources.testValue)
+
 class App extends Component {
   constructor() {
     super();
