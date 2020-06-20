826476640d336576e0d9af6015041d171ff93240
David <djknit@gmail.com>
2020-06-20 18:12:34 -0500

    more work on edit account modal

--- client/src/components/MainApp/EditAccountModal/index.js
+++ client/src/components/MainApp/EditAccountModal/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import getStyle from './style';
 import { profileService } from '../../../data';
-import { api, constants, changeHandlerFactoryFactory } from '../utilities';
+import {
+  api, constants, changeHandlerFactoryFactory, capitalizeFirstLetter
+} from '../utilities';
 import ModalSkeleton from '../../ModalSkeleton';
 import Button from '../../Button';
 import Notification, { NotificationText } from '../../Notification';
@@ -22,6 +24,7 @@ const startingState = {
   unavailableEmails: [],
   secondsUntilRedirect: undefined
 };
+const formId = 'edit-account-form';
 function getTakenUsernameDisplayMessage(username) {
   return `The username "${username}" is not available.`;
 }
@@ -29,12 +32,40 @@ function getTakenEmailDisplayMessage(email) {
   return `There is already an account for the email address "${email}".`;
 }
 const { secondsToDelayRedirect, stepSizeOfRedirectDelay } = constants;
+// function getVariableAttributes(propToEditName) {
+//   if (propToEditName === 'username') {
+//     return {
+      
+//     };
+//   }
+//   else if (propToEditName === 'email') {
+//     return {};
+//   }
+//   else if (propToEditName === 'password') {
+//     return {};
+//   }
+//   else throw new Error('invalid account prop name');
+// }
 
 class _EditAccountModal_needsData extends Component {
   constructor(props) {
     super(props);
     this.changeHandlerFactory = changeHandlerFactoryFactory().bind(this);
+    this.reset = this.reset.bind(this);
     this.state = { ...startingState };
+  };
+
+  afterChange(propName) { // `propName` is the name of property (of state of this component) that was changed
+
+  };
+
+  getInputProblems(problemsToKeep, problemMessagesToKeep) {
+    let problems = problemsToKeep || {};
+    let problemMessages = problemMessagesToKeep || [];
+  };
+
+  reset() {
+
   };
 
   render() {
@@ -42,8 +73,10 @@ class _EditAccountModal_needsData extends Component {
     const { props, state, changeHandlerFactory } = this;
 
     const {
-      user, isActive
+      user, isActive, propToEditName, closeModal
     } = props;
+    const propToEditCurrentValue = user && user[propToEditName];
+    const capPropToEditName = capitalizeFirstLetter(propToEditName);
 
     const {
       updatedAccountProp,
@@ -61,8 +94,36 @@ class _EditAccountModal_needsData extends Component {
 
     return (
       <ModalSkeleton
-        // {...{ isActive }}
-
+        {...{
+          isActive,
+          closeModal
+        }}
+        title={`${propToEditCurrentValue ? 'Edit' : 'Add'} ${capPropToEditName}`}
+        isCloseButtonDisabled={hasSuccess}
+        footerContent={
+          <>
+            <Button
+              color="light"
+              onClick={() => {
+                this.reset();
+                closeModal();
+              }}
+              disabled={isLoading || hasSuccess}
+            >
+              Cancel
+            </Button>
+            <Button
+              color={hasSuccess ? 'success' : 'primary'}
+              onClick={this.submit}
+              disabled={isLoading || hasSuccess || !updatedAccountProp || !verifyPassword}
+              formId={formId}
+              isSubmit
+              isLoading={isLoading}
+            >
+              Submit
+            </Button>
+          </>
+        }
       >
         
 
--- client/src/utilities/index.js
+++ client/src/utilities/index.js
@@ -3,11 +3,17 @@ import api from './api';
 import dates from './dates';
 import jobData from './jobData';
 
+function capitalizeFirstLetter(string) {
+  // source: https://stackoverflow.com/questions/1026069/how-do-i-make-the-first-letter-of-a-string-uppercase-in-javascript
+  return string && (string.charAt(0).toUpperCase() + string.slice(1));
+}
+
 export {
   dataServiceFactory,
   api,
   dates,
-  jobData
+  jobData,
+  capitalizeFirstLetter
 };
 
 export default {
